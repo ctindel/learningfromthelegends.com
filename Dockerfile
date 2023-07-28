b23b09877fe3c7a4eb4f5d3e6bd67d6ef0b658b3
ctindel <chad@tindel.net>
2023-07-28 15:57:47 -0400

    Updated Dockerfile and s3 bucket

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,32 @@
-FROM --platform=linux/amd64 ubuntu:16.04
+FROM ubuntu:20.04
+
+ARG TARGETPLATFORM
+
 ENV SHELL /bin/bash
 ENV GOPATH=/root/go
 
 USER root
 
 RUN apt-get -y update && \
-    apt-get install -y lsb-release iproute2 sudo vim curl git make build-essential awscli && \
     mkdir -p /tmp/lftl && \
-    curl https://storage.googleapis.com/golang/go1.19.5.linux-amd64.tar.gz -o /tmp/lftl/go1.19.5.linux-amd64.tar.gz && \
-    tar zxpvf /tmp/lftl/go1.19.5.linux-amd64.tar.gz -C /usr/local && \
-    cd /tmp/lftl && \
+    apt-get install -y lsb-release sudo vim curl git make build-essential
+
+RUN echo "Targetplatform is ${TARGETPLATFORM}"
+
+RUN if [ "$TARGETPLATFORM" = "linux/amd64" ]; then ARCHITECTURE=linux-amd64; elif [ "$TARGETPLATFORM" = "linux/arm64" ]; then ARCHITECTURE=linux-arm64; else exit 1; fi && \
+ curl https://storage.googleapis.com/golang/go1.19.5.${ARCHITECTURE}.tar.gz -o /tmp/lftl/go.${ARCHITECTURE}.tar.gz && \
+    tar zxpvf /tmp/lftl/go.${ARCHITECTURE}.tar.gz -C /usr/local
+
+RUN cd /tmp/lftl && \
     git clone https://github.com/gohugoio/hugo.git && \
     cd hugo && \
     /usr/local/go/bin/go install --tags extended && \
     echo "#!/bin/bash\ncd /mnt/lftl; /root/go/bin/hugo server -w --bind 0.0.0.0 -b http://localhost:8080/ --disableFastRender --appendPort=false" > /tmp/lftl/run_local.sh && \
     chmod 755 /tmp/lftl/run_local.sh && \
     echo "#!/bin/bash\necho \"Run 'docker exec -it lftl_shell /bin/bash'\"\n echo \"Press [CTRL+C] to stop..\"\nwhile true\ndo\n   sleep 1\ndone" > /tmp/lftl/run_shell.sh && \
-    chmod 755 /tmp/lftl/run_shell.sh
+    chmod 755 /tmp/lftl/run_shell.sh && \
+    echo "#!/bin/bash\ncd /mnt/lftl; /root/go/bin/hugo && /root/go/bin/hugo deploy\n" > /tmp/lftl/deploy.sh && \
+    chmod 755 /tmp/lftl/deploy.sh
 
 CMD ["/bin/bash"]
 ENTRYPOINT ["/bin/bash", "-c"]
--- utils/lara_links.sh
+++ utils/lara_links.sh
@@ -3,20 +3,20 @@
 EXPIRES_SECS="604800"
 
 declare -A BRUCH
-BRUCH["Bruch_Accompaniment"]="s3://lftl.co/LSJ/HD/LFTL Lara St. John Bruch Accompaniment 20150226.mp4"
-BRUCH["Bruch_Bonus"]="s3://lftl.co/LSJ/HD/LFTL Lara St. John Bruch Bonus 20150223.mp4"
-BRUCH["Bruch_Movement_1"]="s3://lftl.co/LSJ/HD/LFTL Lara St. John Bruch Mvt 1 20150223.mp4"
-BRUCH["Bruch_Movement_2"]="s3://lftl.co/LSJ/HD/LFTL Lara St. John Bruch Mvt 2 20150223.mp4"
-BRUCH["Bruch_Movement_3"]="s3://lftl.co/LSJ/HD/LFTL Lara St. John Bruch Mvt 3 20150223.mp4"
-BRUCH["Bruch_Performance"]="s3://lftl.co/LSJ/HD/LFTL Lara St. John Bruch Performance 20150223.mp4"
+BRUCH["Bruch_Accompaniment"]="s3://learningfromthelegends.com/LSJ/HD/LFTL Lara St. John Bruch Accompaniment 20150226.mp4"
+BRUCH["Bruch_Bonus"]="s3://learningfromthelegends.com/LSJ/HD/LFTL Lara St. John Bruch Bonus 20150223.mp4"
+BRUCH["Bruch_Movement_1"]="s3://learningfromthelegends.com/LSJ/HD/LFTL Lara St. John Bruch Mvt 1 20150223.mp4"
+BRUCH["Bruch_Movement_2"]="s3://learningfromthelegends.com/LSJ/HD/LFTL Lara St. John Bruch Mvt 2 20150223.mp4"
+BRUCH["Bruch_Movement_3"]="s3://learningfromthelegends.com/LSJ/HD/LFTL Lara St. John Bruch Mvt 3 20150223.mp4"
+BRUCH["Bruch_Performance"]="s3://learningfromthelegends.com/LSJ/HD/LFTL Lara St. John Bruch Performance 20150223.mp4"
 
 declare -A MENDELSSOHN
-MENDELSSOHN["Mendelssohn_Accompaniment"]="s3://lftl.co/LSJ/HD/LFTL Lara St. John Mendelssohn Accompaniment 20150226.mp4"
-MENDELSSOHN["Mendelssohn_Bonus"]="s3://lftl.co/LSJ/HD/LFTL Lara St. John Mendelssohn Bonus 20150223.mp4"
-MENDELSSOHN["Mendelssohn_Movement_1"]="s3://lftl.co/LSJ/HD/LFTL Lara St. John Mendelssohn Mvt 1 20150223.mp4"
-MENDELSSOHN["Mendelssohn_Movement_2"]="s3://lftl.co/LSJ/HD/LFTL Lara St. John Mendelssohn Mvt 2 20150223.mp4"
-MENDELSSOHN["Mendelssohn_Movement_3"]="s3://lftl.co/LSJ/HD/LFTL Lara St. John Mendelssohn Mvt 3 20150223.mp4"
-MENDELSSOHN["Mendelssohn_Performance"]="s3://lftl.co/LSJ/HD/LFTL Lara St. John Mendelssohn Performance 20150226.mp4"
+MENDELSSOHN["Mendelssohn_Accompaniment"]="s3://learningfromthelegends.com/LSJ/HD/LFTL Lara St. John Mendelssohn Accompaniment 20150226.mp4"
+MENDELSSOHN["Mendelssohn_Bonus"]="s3://learningfromthelegends.com/LSJ/HD/LFTL Lara St. John Mendelssohn Bonus 20150223.mp4"
+MENDELSSOHN["Mendelssohn_Movement_1"]="s3://learningfromthelegends.com/LSJ/HD/LFTL Lara St. John Mendelssohn Mvt 1 20150223.mp4"
+MENDELSSOHN["Mendelssohn_Movement_2"]="s3://learningfromthelegends.com/LSJ/HD/LFTL Lara St. John Mendelssohn Mvt 2 20150223.mp4"
+MENDELSSOHN["Mendelssohn_Movement_3"]="s3://learningfromthelegends.com/LSJ/HD/LFTL Lara St. John Mendelssohn Mvt 3 20150223.mp4"
+MENDELSSOHN["Mendelssohn_Performance"]="s3://learningfromthelegends.com/LSJ/HD/LFTL Lara St. John Mendelssohn Performance 20150226.mp4"
 
 for key in ${!BRUCH[@]}; do
     presignUrl=$(aws s3 presign "${BRUCH[${key}]}" --expires-in $EXPIRES_SECS)
